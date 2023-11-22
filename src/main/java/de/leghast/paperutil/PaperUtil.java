87162405b07d63c4a0e1bf6d6d60f7aac8c8081a
GhastCraftHD <ghastcrafthd@troense.de>
2023-11-22 19:33:28 +0100

    Initial commit

---
+++ src/main/java/de/leghast/paperutil/PaperUtil.java
@@ -0,0 +1,17 @@
+package de.leghast.paperutil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperUtil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
